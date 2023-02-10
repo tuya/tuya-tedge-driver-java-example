70502f68853e6fa56f52d3ba209986f990880144
zhangcunli <zhangcl@tuya.com>
2023-02-10 14:04:22 +0800

    update: readme_cn

--- src/main/java/dpdemo/dpdriver/DpDriverImpl.java
+++ src/main/java/dpdemo/dpdriver/DpDriverImpl.java
@@ -10,7 +10,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
